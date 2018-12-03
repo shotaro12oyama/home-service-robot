eb54f452ac346f626da4cfe6119b57f6c2471392
Shotaro Oyama <shotaro12oyama@gmail.com>
2018-12-03 10:25:48 +0900

    update

--- ShellScripts/add_markers.sh
+++ ShellScripts/add_markers.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-#xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/simple_environment.world" &
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find wall_follower)/../World/simple_environment.world" &
 sleep 5
-#xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/map/map.yaml" &
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find wall_follower)/../World/map/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/simple_environment.world" &
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find wall_follower)/../World/simple_environment.world" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
